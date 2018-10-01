f366361c9b85a301e11b498dbd3a9f7dd87cefc7
suenweek <roman.novatorov@gmail.com>
2018-10-01 19:36:00 +0300

    v0.0.5:  - Reduce default q_size to 0  - Reduce number of consumers to 1  - Start event handlers as consumer children tasks

--- src/async_vk_bot/dispatcher.py
+++ src/async_vk_bot/dispatcher.py
@@ -4,17 +4,17 @@ import trio
 
 class Dispatcher:
 
-    def __init__(self, event_generator, q_size=1, n_workers=1):
+    def __init__(self, event_generator, q_size=0):
         self._event_generator = event_generator
         self._events = trio.Queue(q_size)
-        self._n_workers = n_workers
         self._handlers = defaultdict(list)
+        self._nursery = None
 
     async def __call__(self):
         async with trio.open_nursery() as nursery:
+            self._nursery = nursery
             nursery.start_soon(self._producer)
-            for _ in range(self._n_workers):
-                nursery.start_soon(self._consumer)
+            nursery.start_soon(self._consumer)
 
     def on(self, event_type):
         """Decorator version of `self.add_handler`."""
@@ -32,8 +32,8 @@ class Dispatcher:
 
     async def _consumer(self):
         async for event in self._events:
-            await self._dispatch(event)
+            self._dispatch(event)
 
-    async def _dispatch(self, event):
+    def _dispatch(self, event):
         for handler in self._handlers[event['type']]:
-            await handler(event['object'])
+            self._nursery.start_soon(handler, event['object'])
