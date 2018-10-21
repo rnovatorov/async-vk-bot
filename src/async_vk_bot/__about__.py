106672613dbd3f4a900ef7a1c8f0a58670b6847f
Roman Novatorov <roman.novatorov@gmail.com>
2018-10-21 23:31:19 +0300

    v0.5.0:  - Add long poll errors handling

--- src/async_vk_bot/__about__.py
+++ src/async_vk_bot/__about__.py
@@ -1,5 +1,5 @@
 __title__ = 'async-vk-bot'
-__version__ = '0.4.4'
+__version__ = '0.5.0'
 __description__ = 'Async VK bot builder'
 __url__ = 'https://github.com/Suenweek/async-vk-bot'
 __author__ = 'Roman Novatorov'
--- src/async_vk_bot/poller.py
+++ src/async_vk_bot/poller.py
@@ -3,47 +3,86 @@ from urllib.parse import urlencode
 import asks
 
 
+DEFAULT_WAIT = 25
+ACT = 'a_check'
+
+ERRNO_HISTORY_OUTDATED = 1
+ERRNO_KEY_EXPIRED = 2
+ERRNO_DATA_LOST = 3
+
+
 class Poller:
 
-    def __init__(self, api):
-        self._api = api
+    def __init__(self, api, wait=DEFAULT_WAIT):
+        self.api = api
+        self.wait = wait
 
-    async def __call__(self):
-        server = await self._get_server()
-        async for events in server:
+        self._group_id = None
+
+        self.server = None
+        self.key = None
+        self.ts = None
+
+    def __call__(self):
+        return self.poll()
+
+    async def poll(self):
+        await self._init()
+        while True:
+            events = await self._wait_events()
             for event in events:
                 yield event
 
-    async def _get_server(self):
-        groups = await self._api.groups.getById()
-        group_id = groups[0]['id']
-        cfg = await self._api.groups.getLongPollServer(group_id=group_id)
-        return Server(**cfg)
+    async def _init(self):
+        config = await self._get_config()
+        self.server = config['server']
+        self.key = config['key']
+        self.ts = config['ts']
 
-
-class Server:
-
-    ACT = 'a_check'
-
-    def __init__(self, server, key, ts, wait=25):
-        self.server = server
-        self.key = key
-        self.ts = ts
-        self.wait = wait
-
-    def __aiter__(self):
-        return self
-
-    async def __anext__(self):
+    async def _wait_events(self):
         url = self._make_url()
         response = await asks.get(url)
         payload = response.json()
-        self.ts = payload['ts']
-        return payload['updates']
+
+        errno = payload.get('failed')
+
+        if errno is None:
+            self.ts = payload['ts']
+            return payload['updates']
+
+        if errno == ERRNO_HISTORY_OUTDATED:
+            self.ts = payload['ts']
+
+        elif errno == ERRNO_KEY_EXPIRED:
+            config = await self._get_config()
+            self.key = config['key']
+
+        elif errno == ERRNO_DATA_LOST:
+            config = await self._get_config()
+            self.key = config['key']
+            self.ts = config['ts']
+
+        else:
+            raise RuntimeError(f'Unexpected errno: {errno}')
+
+        return []
+
+    async def _get_config(self):
+        group_id = await self._get_group_id()
+        config = await self.api.groups.getLongPollServer(
+            group_id=group_id
+        )
+        return config
+
+    async def _get_group_id(self):
+        if self._group_id is None:
+            groups = await self.api.groups.getById()
+            self._group_id = groups[0]['id']
+        return self._group_id
 
     def _make_url(self):
         query = urlencode({
-            'act': self.ACT,
+            'act': ACT,
             'key': self.key,
             'ts': self.ts,
             'wait': self.wait
