bf7f4e2f8a67fd7c1e1883d940a1fee74d874ec1
Roman Novatorov <roman.novatorov@gmail.com>
2019-04-21 00:05:10 +0300

    Pin `async-vk-api` version to 0.6.1

--- setup.py
+++ setup.py
@@ -3,12 +3,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='async-vk-bot',
-    version='0.9.4',
+    version='0.9.5',
     description='Async VK bot builder',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     url='https://github.com/Suenweek/async-vk-bot',
     author='Roman Novatorov',
     author_email='roman.novatorov@gmail.com',
-    install_requires=['async-vk-api>=0.6.0', 'async_generator']
+    # TODO: Bump version when asks gets session instantiation fixed:
+    #   https://github.com/theelous3/asks/pull/120
+    install_requires=['async-vk-api==0.6.1', 'async_generator']
 )
